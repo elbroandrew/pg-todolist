4c9f42632fd046be41a153fb4ccd82a7d0655c77
Andrew <elbroandrew@gmail.com>
2025-10-27 17:31:10 +1000

    feat: пилю из монолита два сервиса

---
+++ internal/router/task_router.go
@@ -0,0 +1,55 @@
+package router
+
+import (
+	"net/http"
+	"pg-todolist/internal/handlers"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Middleware для извлечения userID из заголовка, который устанавливает Gateway
+func UserIdMiddleware() gin.HandlerFunc{
+	return func(c *gin.Context){
+		userIDHeader := c.GetHeader("X-User-ID")
+		if userIDHeader == "" {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+				"error": "User ID not provided by gateway"})
+			return 
+		}
+		// валидация ID
+		userID, err := strconv.ParseUint(userIDHeader, 10, 32)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error":"Invalid user id format"})
+			return 
+		}
+
+		c.Set("userID", uint(userID))
+		c.Next()
+	}
+}
+
+func SetupTaskServiceRouter(taskHandler *handlers.TaskHandler) *gin.Engine {
+	//использую gin.New - чтоб наглядно видеть recovery
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	
+	tasksGroup := r.Group("/tasks")
+	tasksGroup.Use(UserIdMiddleware())
+	{
+		tasksGroup.GET("", taskHandler.GetTasks)
+		tasksGroup.POST("", taskHandler.CreateTask)
+		tasksGroup.GET("/:id", taskHandler.GetTaskByID)
+		tasksGroup.PUT("/:id", taskHandler.UpdateTask)
+		tasksGroup.DELETE("/:id", taskHandler.DeleteTask)
+	}
+
+	//curl -X GET http://localhost:8080/health
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "Task Service is healthy"})
+	})
+
+	return r
+}
---
+++ pkg/server/server.go
@@ -0,0 +1,69 @@
+package server
+
+//данный файл нужен для Graceful shutdown, если происходит остановка сервиса через ctrl+C, либо по неизвестно причине.
+//Неизвестная причина может кинуть панику, что перехватит gin.Recovery. Который создан через gin.Default.
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+//приложение, зависимости которого (redis cache, MySQL DB) надо закрыть
+type App struct {
+	HttpServer *http.Server
+	Closers []func() error  //слайс функций для закрытия ресурсов (БД, redis)
+}
+
+func NewApp(handler http.Handler, port string) *App {
+	return &App{
+		HttpServer: &http.Server{
+			Addr: ":"+port,
+			Handler: handler,
+		},
+		Closers: []func() error{},
+	}
+}
+
+// метод закрывающий ресурс
+func (a *App) AddCloser(closer func() error) {
+	a.Closers = append(a.Closers, closer)
+}
+
+// Run запускает приложение и делает graceful shutdown, когда необходимо
+func (a *App) Run() {
+	//запускаю сервер в горутине, чтобы не блокировать код в main, это позволит коду в main дойти до 
+	// той части, где он ждёт сигнала о завершении (<-quit) и выолнить логику Graceful Shutdown.
+	go func(){
+		log.Printf("Server is running on port %s", a.HttpServer.Addr)
+		if err := a.HttpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	//ожидаю сигнал остановки Sigint (ctrl+C), код 2
+	quit := make(chan os.Signal, 1) // 1 - размер буфера, рекоммендация разработчика
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	//Даю время на завершение запросов
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := a.HttpServer.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	//Закрываю ресурсы
+	for _, closer := range a.Closers {
+		if err := closer(); err != nil {
+			log.Printf("Error closing resource: %v", err)
+		}
+	}
+	log.Println("Server exiting")
+}
